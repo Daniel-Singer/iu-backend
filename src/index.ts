fc90b2f28cdeca91403e43343a0ff673f832318e
Daniel <singer.daniel85@gmail.com>
2024-06-11 08:40:31 +0200

    implemented routes in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,36 @@
 import express, { Express } from 'express';
 import colors from 'colors';
 
+// routing imports
+
+import categoryRoutes from './routes/category.routes';
+import commentRoutes from './routes/comment.routes';
+import courseRoutes from './routes/course.routes';
+import issueRoutes from './routes/issue.routes';
+import mediaRoutes from './routes/media.routes';
+import messageRoutes from './routes/message.routes';
+import statusRoutes from './routes/status.routes';
+import userRoutes from './routes/user.routes';
+
+// app setup
 const app: Express = express();
 
 const PORT = process.env.PORT || 8000;
 
-// For parsing request body
+// parsing request body
 app.use(express.json());
 
+// use routes
+app.use('/api/v1/categories', categoryRoutes);
+app.use('/api/v1/comments', commentRoutes);
+app.use('/api/v1/courses', courseRoutes);
+app.use('/api/v1/issues', issueRoutes);
+app.use('/api/v1/media', mediaRoutes);
+app.use('/api/v1/messages', messageRoutes);
+app.use('/api/v1/status', statusRoutes);
+app.use('/api/v1/users', userRoutes);
+
+// starting server
 app.listen(PORT, () =>
   console.log(colors.bgGreen(`SERVER running on port ${PORT}`))
 );
