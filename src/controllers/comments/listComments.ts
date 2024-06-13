dc6f07ff0d73c9f20790eb4895a0db97ab41d3a7
Daniel <singer.daniel85@gmail.com>
2024-06-13 08:41:00 +0200

    comments have created_from information included

--- src/controllers/comments/listComments.ts
+++ src/controllers/comments/listComments.ts
@@ -16,12 +16,39 @@ export const listComments = async (
 ) => {
   try {
     const { issue_id } = req.params;
-    const comments = await db
-      .select('*')
-      .from('comment')
+
+    const comments = await db('comment')
+      .join('users', 'created_from', 'users.id')
+      .select(
+        'comment.id',
+        'comment.text',
+        'comment.issue_id',
+        'users.id as user_id',
+        'users.first_name as user_first_name',
+        'users.last_name as user_last_name',
+        'comment.created_at',
+        'comment.updated_at'
+      )
       .where('issue_id', issue_id);
 
-    res.status(200).json(comments);
+    if (!comments) {
+      res.status(404);
+      throw new Error('Keine Kommentare für Fehlermeldung vorhanden');
+    } else {
+      const formatted = comments?.map((comment) => ({
+        id: comment.id,
+        text: comment.text,
+        issue_id: comment.issue_id,
+        created_from: {
+          id: comment.user_id,
+          first_name: comment.user_first_name,
+          last_name: comment.user_last_name,
+        },
+        created_at: comment.created_at,
+        updated_at: comment.updated_at,
+      }));
+      res.status(200).json(formatted);
+    }
   } catch (error) {
     next(error);
   }
---
+++ src/types/Comment.d.ts
@@ -0,0 +1,24 @@
+interface ICommentBase {
+  id?: number;
+  text: string;
+  created_at?: Date;
+  updated_at?: Date;
+}
+
+// Interface used when comment is created
+
+interface ICommentCreate extends ICommentBase {
+  created_from: number;
+  issue_id: number;
+}
+
+// Interface used when comments are queried
+
+interface ICommentReceive extends ICommentBase {
+  created_from: {
+    id: number;
+    first_name: string;
+    last_name: string;
+  };
+  issue_id: number;
+}
