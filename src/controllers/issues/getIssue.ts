a5463f09068c1801c336aad8b01e3dc92cb75aa9
Daniel <singer.daniel85@gmail.com>
2024-06-26 07:09:05 +0200

    updated getIssue controller

---
+++ src/controllers/issues/getIssue.ts
@@ -0,0 +1,54 @@
+import { Request, Response, NextFunction } from 'express';
+import { db } from '../../config/db';
+
+/**
+ * getIssue
+ *
+ * @description     Returns single issue row from database
+ * @route           GET /api/v1/issues/:id
+ * @access          Private, Owner, Admin
+ */
+
+export const getIssue = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    // TODO - Implement condition to check if auth user is owner, assigned tutor or admin
+    const issue = await db('issue')
+      .select([
+        'issue.*',
+        'course.code as course_code',
+        'course.title as course_title',
+        'course.tutor_id as course_tutor_id',
+        'users.id as tutor_id',
+        'users.first_name as tutor_first_name',
+        'users.last_name as tutor_last_name',
+      ])
+      .leftJoin('course', 'issue.course_id', 'course.id')
+      .leftJoin('users', 'course.tutor_id', 'users.id')
+      .where('issue.id', req.params.id)
+      .first();
+    const formatted = {
+      id: issue.id,
+      title: issue.title,
+      description: issue.description,
+      course: {
+        id: issue.course_id,
+        code: issue.course_code,
+        title: issue.course_title,
+        tutor: {
+          id: issue.course_tutor_id,
+          first_name: issue.tutor_first_name,
+          last_name: issue.tutor_last_name,
+        },
+      },
+      created_at: issue.created_at,
+      updated_at: issue.updated_at,
+    };
+    res.status(200).json(formatted);
+  } catch (error) {
+    next(error);
+  }
+};
--- src/routes/issue.routes.ts
+++ src/routes/issue.routes.ts
@@ -4,12 +4,13 @@ import { createIssue } from '../controllers/issues/createIssue';
 import { deleteIssue } from '../controllers/issues/deleteIssue';
 import { isAdmin } from '../middleware/auth/isAdmin';
 import { listUserIssues } from '../controllers/issues/listUserIssues';
+import { getIssue } from '../controllers/issues/getIssue';
 
 const router: Router = exress.Router();
 
 router.route('/').post(createIssue);
 router.route('/admin').get(isAdmin, listIssues);
 router.route('/user').get(listUserIssues);
-router.route('/:id').get().delete(deleteIssue).put();
+router.route('/:id').get(getIssue).delete(deleteIssue).put();
 
 export default router;
