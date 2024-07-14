59a218978b20f335c335f8f0a7690ca5c5e87c6f
Daniel <singer.daniel85@gmail.com>
2024-07-14 13:45:39 +0200

    analytics controller added that creates object with infos regarding related comments of user

--- src/controllers/analytics/comments/getCommentStatusByUserIssues.ts
+++ src/controllers/analytics/comments/getCommentStatusByUserIssues.ts
@@ -16,11 +16,46 @@ export const getCommentStatusByUserIssues = async (
 ) => {
   try {
     const { auth } = req.body;
-    // find all issues related to user
+
+    // find all issues related to user if user is creator or assignee
     const issues: IIssueReceive[] = await db('issue')
       .where('created_from', auth.id)
       .orWhere('assigned_to', auth.id);
-    res.status(200).json(issues);
+
+    // get all issue ids
+    const issueIds = issues.map(({ id }) => id!);
+
+    // find all related comments for all issues
+    const comments = await db('comment').whereIn('issue_id', issueIds);
+
+    // get all comment ids
+    const commentIds = comments.map(({ id }) => id!);
+
+    // check if the user has seen the comments yet
+    const seenByUser = await db('comment_seen_by_user')
+      .select(['comment_id'])
+      .whereIn('comment_id', commentIds);
+
+    // get commentIds array of seenByUser
+    const seenByUserCommentIds = seenByUser.map(
+      ({ comment_id }) => comment_id!
+    );
+
+    const allCommentIds = [...commentIds, ...seenByUserCommentIds];
+
+    // get ids of comments that user has not seen yet
+
+    const hasNotSeenComments = allCommentIds.filter(
+      (id) => !(commentIds.includes(id) && seenByUserCommentIds.includes(id))
+    );
+
+    res.status(200).json({
+      comments: {
+        new: {
+          count: hasNotSeenComments.length!,
+        },
+      },
+    });
   } catch (error) {
     next(error);
   }
