542782dc4e811333fb5a757e7ba4f1d8e3c3165f
Daniel <singer.daniel85@gmail.com>
2024-08-28 14:02:22 +0200

    path gets logged

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -9,6 +9,7 @@ const PROD_PATH = path.join('/', 'home', 'iu', 'uploads');
 
 const PATH = process.env.NODE_ENV === 'development' ? DEV_PATH : PROD_PATH;
 
+console.log(PATH);
 const upload = multer({ dest: PATH });
 
 export default upload;
