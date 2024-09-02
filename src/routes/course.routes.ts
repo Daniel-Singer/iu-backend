46c67661f883c103ac6c545c2aee2a31dcf3a20c
Daniel <singer.daniel85@gmail.com>
2024-09-02 10:33:21 +0200

    course can be added from tutor

--- src/routes/course.routes.ts
+++ src/routes/course.routes.ts
@@ -6,10 +6,14 @@ import { getCourse } from '../controllers/courses/getCourse';
 import { deleteCourse } from '../controllers/courses/deleteCourse';
 import { hasRequiredData } from '../middleware/validation/course/hasRequiredData';
 import { createCourse } from '../controllers/courses/createCourse';
+import { isAdminOrTutor } from '../middleware/auth/isAdminOrTutor';
 
 const router: Router = exress.Router();
 
-router.route('/').get(listCourses).post(isAdmin, hasRequiredData, createCourse);
+router
+  .route('/')
+  .get(listCourses)
+  .post(isAdminOrTutor, hasRequiredData, createCourse);
 router
   .route('/:id')
   .get(getCourse)
